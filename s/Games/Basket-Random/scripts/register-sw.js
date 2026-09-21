4dff6f05ef9fab7fa340b5fe886696abd415dadf
YTRellVr <R4llalt@gmail.com>
2026-09-21 16:32:50 -0400

    Register Basket-Random service worker from deployed URL

--- s/Games/Basket-Random/scripts/register-sw.js
+++ s/Games/Basket-Random/scripts/register-sw.js
@@ -1,1 +1,14 @@
-"use strict";window.C3_RegisterSW=async function(){if(navigator.serviceWorker)try{const a=await navigator.serviceWorker.register("sw.js",{scope:"./"});console.info("Registered service worker on "+a.scope)}catch(a){console.warn("Failed to register service worker: ",a)}};
+"use strict";
+window.C3_RegisterSW = async function() {
+	if (navigator.serviceWorker) {
+		try {
+			const registration = await navigator.serviceWorker.register(
+				"https://totallymathstuff.github.io/s/Games/Basket-Random/sw.js",
+				{ scope: "https://totallymathstuff.github.io/s/Games/Basket-Random/" }
+			);
+			console.info("Registered service worker on " + registration.scope);
+		} catch (error) {
+			console.warn("Failed to register service worker: ", error);
+		}
+	}
+};
